c5853dbffde7b876202cf183951b1084edad704d
Gustavo Dos Santos <simondossantos@gmail.com>
2024-10-01 15:14:11 -0300

    add 🆕: nuevos metadatos añadidos en la pagina.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,23 @@ import "./globals.css";
 export const metadata: Metadata = {
 	title: "Interactive Comments",
 	description: "Interactive Comments with Next.js",
+	twitter: {
+		images: "/assets/images/social-preview.png",
+		card: "summary_large_image",
+		creator: "@juliusomo",
+		site: "@juliusomo",
+		title: "Interactive Comments with Next.js",
+		description: "Interactive Comments with Next.js",
+	},
+	openGraph: {
+		images: "/assets/images/social-preview.png",
+		type: "website",
+		url: "https://interactive-comments-nextjs-6cnxsd2dp.vercel.app/",
+		siteName: "Interactive Comments with Next.js",
+		title: "Interactive Comments with Next.js",
+		description: "Interactive Comments with Next.js",
+	}
+		
 };
 
 export default function RootLayout({
